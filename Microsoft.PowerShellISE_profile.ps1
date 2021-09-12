572860c5f2305ae1567bff87cf064d1dabe0eb57
BooMShanKerX <boomshankerx@hotmail.com>
2021-09-12 09:11:23 -0600

    init

---
+++ Microsoft.PowerShellISE_profile.ps1
@@ -0,0 +1,7 @@
+﻿# Version 1.0
+
+Function F_PATH {$env:path -split ';'}
+New-Alias -Name path -Value F_PATH  -Force
+
+Function F_ENV {gci env:* | sort name}
+New-Alias -Name lenv -Value F_ENV  -Force
---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,20 @@
+﻿# Version 1.0
+
+Function F_ENV {gci env:* | sort name}
+New-Alias -Name lenv -Value F_ENV  -Force
+
+Function F_NETSTAT_LISTEN { Get-NetTCPConnection -State Listen }
+New-Alias -Name listen -Value F_NETSTAT_LISTEN -Force
+
+Function F_IP { Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Wi-Fi,LAN} 
+New-Alias -Name ip -Value F_IP -Force
+
+Function F_PATH {$env:path -split ';'}
+New-Alias -Name path -Value F_PATH  -Force
+
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
